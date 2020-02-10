8b97ce06b96983616fb65dd11c14c20d0ed793fa
hafif07 <mhafif29@gmail.com>
2020-02-10 16:47:14 +0700

    change controller seleksi dan view seleksi

--- application/controllers/Seleksi.php
+++ application/controllers/Seleksi.php
@@ -4,7 +4,7 @@ class Seleksi extends CI_Controller{
 	public function index()
 	{	$data["page"] = "seleksi";
 		$this->load->view("templates/header",$data);
-		$this->load->view("beranda");
+		$this->load->view("seleksi");
 		$this->load->view("templates/footer");
 	}
 }
---
+++ application/views/seleksi.php
@@ -0,0 +1,3 @@
+<div style="display: flex;height: 95vh;width: 100%;justify-content: center;align-items: center">
+	
+</div>
