1a6bad619d3d1ceb56e9f7544d35fc40b9adecb9
hafif07 <mhafif29@gmail.com>
2020-02-13 08:38:12 +0700

    add view soal ujian

--- application/controllers/Ujian.php
+++ application/controllers/Ujian.php
@@ -3,6 +3,12 @@
 class Ujian extends CI_Controller{
 	public function login()
 	{
+		$data["kodedefault"]= "seleksijurusan";
+		if (isset($_POST["masuk"])){
+			if($data["kodedefault"] == $_POST["kode"]){
+				$data["kodedefault"] ="asdfnkasdfksdafkasdfjkas";
+			}
+		}
 		$data["page"] = "ujian";
 		$this->load->view("templates/header",$data);
 		$this->load->view("ujian");
--- application/views/soalujian.php
+++ application/views/soalujian.php
@@ -1,7 +1,7 @@
 <div style="display: flex;height: 95vh;width: 100%;justify-content: center;align-items: center">
 	<form method="post" action="">
 		<div class="form-group">
-			<label for="kode">Masukan kode ujian</label>
+			<label for="kode">soalll ujiannn</label>
 			<input type="text" class="form-control" id="kode" name="kode">
 		</div>
 		<button type="submit" class="btn btn-primary" name="masuk">Masuk</button>
