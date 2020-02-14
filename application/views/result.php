ed050a0cbaace08f7a47024de2856e7adc1bc5b4
hafif07 <mhafif29@gmail.com>
2020-02-14 14:00:13 +0700

    perubahan controller ujian/result

--- application/views/result.php
+++ application/views/result.php
@@ -1,9 +1,3 @@
 <div style="display: flex;height: 95vh;width: 100%;justify-content: center;align-items: center">
-	<form method="post" action="">
-		<div class="form-group">
-			<label for="kode">Masukan kode ujian</label>
-			<input type="text" class="form-control" id="kode" name="kode">
-		</div>
-		<button type="submit" class="btn btn-primary" name="masuk">Masuk</button>
-	</form>
+	
 </div>
