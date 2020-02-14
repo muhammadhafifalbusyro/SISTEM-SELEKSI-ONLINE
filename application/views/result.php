dcf6d56b581330955b0b881d5e7a301fc528c161
hafif07 <mhafif29@gmail.com>
2020-02-14 14:43:38 +0700

    perubahan controller rujian/result

--- application/views/result.php
+++ application/views/result.php
@@ -1,4 +1,10 @@
 <div style="display: flex;height: 95vh;width: 100%;justify-content: center;align-items: center">
-	<h1>hasil ujian</h1>
-	<h2><?php echo $hasil ?></h2>
+	<h3>Nama Lengkap :</h3>
+	<h3><?php echo $nama_lengkap ?></h3><br>
+	<h3>Pilihan Jurusan 1 :</h3>
+	<h3><?php echo $jurusan1 ?></h3><br>
+	<h3>Pilihan Jurusan 2 :</h3>
+	<h3><?php echo $jurusan2 ?></h3><br>
+	<h3>Hasil Ujian :</h3>
+	<h3><?php echo $hasil ?></h3><br>
 </div>
