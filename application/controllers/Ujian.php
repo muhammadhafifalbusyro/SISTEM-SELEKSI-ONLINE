33eaf9a45ffb3bbd1729e81cf94395fa616c87f5
hafif07 <mhafif29@gmail.com>
2020-02-15 09:43:34 +0700

    add session

--- application/controllers/Ujian.php
+++ application/controllers/Ujian.php
@@ -5,13 +5,18 @@ class Ujian extends CI_Controller{
 	{
 		parent::__construct();
 		$this->load->model("model_peserta");
+		$this->load->library("session");
 	}
 	public function login()
 	{
+		if(isset($_SESSION["login"])){
+			redirect(base_url()."/ujian/soalujian");
+		}
 		$data["kodedefault"]= "seleksijurusan";
 		$data["alert"] = "";
 		if (isset($_POST["masuk"])){
 			if($data["kodedefault"] == $_POST["kode"]){
+				$_SESSION["login"] = true;
 				redirect(base_url()."/ujian/soalujian");
 			}
 			else{
@@ -26,6 +31,9 @@ class Ujian extends CI_Controller{
 
 	public function soalujian()
 	{
+		if(!isset($_SESSION["login"])){
+			redirect(base_url()."/ujian/login");
+		}
 		$data["page"] = "ujian";
 		$this->load->view("templates/header",$data);
 		$this->load->view("soalujian");
@@ -39,7 +47,7 @@ class Ujian extends CI_Controller{
 		$data["jurusan2"] = " ";
 		$data["hasil"] = 0;
 
-		if (isset($_POST["add"])){
+		if (isset($_POST["add"])&&isset($_SESSION["login"])){
 			$id = null;
 			$data["nama_lengkap"] = $_POST["namalengkap"];
 			$data["jurusan1"] = $_POST["jurusan1"];
@@ -49,10 +57,23 @@ class Ujian extends CI_Controller{
 
 			$this->model_peserta->tambah_data($id, $data);
 
+			$_SESSION["nama_lengkap"] = $_POST["namalengkap"];
+			$_SESSION["jurusan1"] = $_POST["jurusan1"];
+			$_SESSION["jurusan2"] = $_POST["jurusan2"];
+			$_SESSION["hasil"] = $data["hasil"];
+
 		}
-		elseif (isset($_POST["kembali"])) {
+		elseif (isset($_POST["kembali"])&&isset($_SESSION["login"])) {
+			$_SESSION= [];
+			session_unset();
+			session_destroy();
+
 			redirect(base_url()."beranda");
 		}
+		elseif(!isset($_SESSION["login"])){
+			redirect(base_url()."ujian/login");
+		}
+
 		$this->load->view("templates/header",$data);
 		$this->load->view("result");
 		$this->load->view("templates/footer");
--- application/views/result.php
+++ application/views/result.php
@@ -1,6 +1,7 @@
 <div style="display: flex;height: 95vh;width: 100%;justify-content: center;align-items: center;flex-direction: column;">
 	<h1>Data Berhasil Terkirim !</h1><br>
-	<div class="container">
+	<?php if(isset($_POST["add"])): ?>
+		<div class="container">
 		<form method="post" action="">
 			<table class="table ">
 				<thead class="thead-dark">
@@ -15,7 +16,7 @@
 					<tr>
 						<td><?php echo $nama_lengkap ?></td>
 						<td><?php echo $jurusan1 ?></td>
-						<td><?php echo $jurusan2; ?></td>
+						<td><?php echo $jurusan2 ?></td>
 						<td><?php echo $hasil ?></td>
 					</tr>
 				</tbody>
@@ -23,4 +24,29 @@
 			<button type="submit" class="btn btn-primary ml-5" name="kembali" id="kembali" >Home</button>
 		</form>
 	</div>
+	<?php elseif (!isset($_POST["add"])):?>
+		<div class="container">
+		<form method="post" action="">
+			<table class="table ">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">Nama Lengkap</th>
+						<th scope="col">Pilihan Jurusan 1</th>
+						<th scope="col">Pilihan Jurusan 2</th>
+						<th scope="col">Hasil Ujian</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?php echo $_SESSION["nama_lengkap"] ?></td>
+						<td><?php echo $_SESSION["jurusan1"] ?></td>
+						<td><?php echo $_SESSION["jurusan2"] ?></td>
+						<td><?php echo $_SESSION["hasil"] ?></td>
+					</tr>
+				</tbody>
+			</table>
+			<button type="submit" class="btn btn-primary ml-5" name="kembali" id="kembali" >Home</button>
+		</form>
+	</div>
+	<?php endif; ?>
 </div>
